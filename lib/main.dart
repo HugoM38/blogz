43211ba1e44a9582b2148d5445f9b92a7ceb3c3c
Hugo <>
2023-12-12 09:04:16 +0100

    chore: Add FirebaseCore package and init Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:blogz/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const Blogz());
 }
 
